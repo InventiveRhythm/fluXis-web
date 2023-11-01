fb7897a1e49e1dab6b3b00463d078705a879a4d0
Flux <flustixshinu@gmail.com>
2023-11-01 04:30:05 +0100

    on my god why is linux case-sensitive

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import { startLoading, stopLoading } from '../utils/loading.js'
+import { startLoading, stopLoading } from '../utils/Loading.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
