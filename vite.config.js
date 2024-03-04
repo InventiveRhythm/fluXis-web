25f0b196041bc3b79594f8227415994b8654b9e0
Flustix <flustix@foxes4life.net>
2024-03-05 00:15:31 +0100

    specify vite port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 2432,
+    strictPort: true
+  },
   plugins: [vue()],
   resolve: {
     alias: {
