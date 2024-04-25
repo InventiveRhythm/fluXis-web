cf7e519a7b133af98697c40c2633f1f86ad41207
flustix <me@flux.moe>
2024-04-25 13:25:47 +0200

    dont send token if the current url doesnt match the sender

--- src/views/account/OAuth.vue
+++ src/views/account/OAuth.vue
@@ -4,8 +4,19 @@ const token = new URLSearchParams(window.location.search).get('token');
 
 if (token) {
     window.addEventListener("message", (event) => {
-        if (!event.source)
+        if (!event.source) {
+            console.log('no source');
             return;
+        }
+
+        console.log('source: ', event.origin);
+        console.log('current: ', window.location.origin);
+
+        // make sure the sender is the same url
+        if (event.origin !== window.location.origin)  {
+            console.log('origin doesn\'t match, aborting for security reasons');
+            return;
+        }
 
 	    event.source.postMessage({ token });
     });
