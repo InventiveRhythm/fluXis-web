b718056b2bab6aa399f58d8faecb0601d22335a0
flustix <me@flux.moe>
2024-08-06 10:03:39 +0200

    re-implement nav dropdowns

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,14 +1,22 @@
 <script setup>
 import { RouterLink, } from "vue-router";
+import { reactive } from "vue";
 
 import FluXisText from "@/assets/images/logo-text.png";
 
 import NavLink from "./NavLink.vue";
 import NavProfile from "./NavProfile.vue";
 
+const react = reactive({
+    showExtra: false,
+    currentLink: "",
+    lastLink: "",
+});
+
 const links = [
     {
         text: "Home",
+        id: "home",
         to: "/",
         icon: "home",
         sublinks: [
@@ -32,6 +40,7 @@ const links = [
     },
     {
         text: "Maps",
+        id: "maps",
         to: "/maps",
         icon: "map",
         sublinks: [
@@ -41,12 +50,13 @@ const links = [
             },
             {
                 text: "Featured Artists",
-                to: "/maps/artists",
+                to: "/artists",
             }
         ],
     },
     {
         text: "Rankings",
+        id: "ranks",
         to: "/rankings/overall",
         icon: "trophy",
         sublinks: [
@@ -70,15 +80,37 @@ const links = [
     },
     {
         text: "Wiki",
+        id: "wiki",
         to: "/wiki",
         icon: "book",
     }
 ];
+
+function linkHovered(link) {
+    const data = links.find(e => e.id == link);
+
+    if (!data || !data.sublinks) {
+        react.showExtra = false;
+        return;
+    }
+
+    react.showExtra = true;
+    react.currentLink = link;
+    react.lastLink = link;
+}
+
+function stopHover() {
+    react.showExtra = false;
+    react.currentLink = "";
+}
 </script>
 
 <template>
     <div class="w-full h-20 fixed top-0 z-50">
-        <div class="size-full flex justify-between items-center bg-dark-2 shadow-normal">
+        <Transition name="nav-fade">
+            <div class="absolute w-full h-56 bg-dark-3" v-if="react.showExtra" ></div>
+        </Transition>
+        <div class="absolute size-full flex justify-between items-center bg-dark-2 shadow-normal">
             <div class="w-24 md:hidden"></div>
             <div class="flex items-center px-3">
                 <RouterLink to="/">
@@ -87,9 +119,27 @@ const links = [
                     </div>
                 </RouterLink>
 
-                <NavLink v-for="link in links" :key="link.to" :to="link.to" :icon="link.icon" :text="link.text" :dropdown="link.sublinks" />
+                <NavLink @mouseover="linkHovered(link.id)" @mouseleave="stopHover" v-for="link in links" :key="link.to" :to="link.to" :icon="link.icon" :text="link.text" :dropdown="link.sublinks" :showDropdown="react.currentLink == link.id" />
             </div>
             <NavProfile />
         </div>
     </div>
 </template>
+
+<style>
+.nav-fade-enter-active,
+.nav-fade-leave-active {
+    transition: all 300ms;
+}
+
+.nav-fade-enter-to,
+.nav-fade-leave-from {
+    opacity: 1;
+    transition-duration: 0ms;
+}
+
+.nav-fade-enter-from,
+.nav-fade-leave-to {
+    opacity: 0;
+}
+</style>
--- src/components/nav/NavLink.vue
+++ src/components/nav/NavLink.vue
@@ -15,69 +15,25 @@ const props = defineProps({
     dropdown: {
         type: Array,
         default: () => []
+    },
+    showDropdown: {
+        type: Boolean,
+        default: false
     }
 });
 </script>
 
 <template>
-    <div class="hidden md:flex items-center px-3 py-1 mx-2 rounded-md transition-colors !bg-opacity-20 hover:bg-white">
+    <div class="hidden md:flex flex-col items-start px-3 py-1 mx-2 rounded-md transition-colors !bg-opacity-20 hover:bg-white">
         <RouterLink :to="to" class="flex gap-2 items-center">
             <i :class="'fa fa-' + icon"></i>
             {{ text }}
         </RouterLink>
-        <!-- i'm gonna completely rewrite this because it sucks -->
-        <!-- <div class="dropdown" v-if="dropdown && dropdown.length > 0">
-            <div class="dropdown-list" >
-                <RouterLink v-for="link in dropdown" :key="link.to" :to="link.to">{{ link.text }}</RouterLink>
+        <div class="p-8 absolute" v-if="showDropdown"></div>
+        <div class="absolute mt-16 group" v-if="dropdown && dropdown.length > 0 && showDropdown">
+            <div class="flex flex-col justify-start items-start gap-1">
+                <RouterLink class="text-sm hover:!opacity-100 group-hover:opacity-60" v-for="link in dropdown" :key="link.to" :to="link.to">{{ link.text }}</RouterLink>
             </div>
-        </div> -->
+        </div>  
     </div>
 </template>
-
-<!-- <style lang="scss">
-.dropdown {
-    position: absolute;
-    top: -60px;
-    left: 50%;
-    transform: translateX(-50%);
-    // width: 200px;
-    min-height: 80px;
-    padding: 70px 20px 20px 20px;
-    visibility: hidden;
-    background-color: var(--bg-secondary);
-    z-index: -10;
-    border-radius: 0 0 20px 20px;
-    backdrop-filter: var(--blur-10);
-    transition: all .3s cubic-bezier(0.22, 1, 0.36, 1);
-    opacity: 0;
-
-    .dropdown-list {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        padding: 0;
-        margin: 0;
-
-        a {
-            padding: 5px 10px;
-            margin: 1px 0;
-            border-radius: calc(var(--border) / 2); 
-            text-align: center;
-            white-space: nowrap;
-        }
-
-        a:hover {
-            background-color: var(--bg-quaternary);
-        }
-    }
-}
-
-.nav-link:hover .dropdown {
-    visibility: visible;
-    opacity: 1;
-    top: -30px;
-}
-</style> -->
