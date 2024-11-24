7a951b421a41eafcce44abeb7915ec94026aae76
flustix <me@flux.moe>
2024-11-24 20:00:06 +0100

    wiki article improvements

---
+++ src/components/markdown/MarkdownView.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { createApp, onMounted, ref, watch, nextTick } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute()
+const router = useRouter();
+
+const props = defineProps<{
+    content: string,
+    scroll?: boolean
+}>()
+
+const target = ref<HTMLDivElement>();
+
+const render = (html: string) => {
+    const ren = createApp({
+        template: html,
+        components: {}
+    })
+
+    ren.use(router)
+
+    if (target.value) {
+        ren.mount(target.value)
+        nextTick(MoveToHash)
+    }
+};
+
+watch(() => props.content, (content) => render(content), { immediate: true })
+onMounted(() => render(props.content))
+
+function MoveToHash() {
+    if (route.hash) {
+        const el = document.getElementById(route.hash.substring(1))
+        if (el) el.scrollIntoView({ block: 'start' })
+    }
+}
+</script>
+
+<template>
+    <div ref="target" class="md-content"></div>
+</template>
+
+
+<style lang="scss">
+.md-content {
+    @apply flex flex-col gap-3 max-w-full;
+
+    h1 {
+        @apply text-2xl;
+    }
+
+    h2 {
+        @apply text-xl;
+    }
+
+    h3 {
+        @apply text-lg;
+    }
+
+    h4 {
+        @apply text-base;
+    }
+
+    a {
+        @apply text-primary hover:underline
+    }
+
+    >p {
+        @apply text-sm;
+    }
+
+    ul,
+    ol {
+        @apply list-inside list-disc;
+
+        li {
+            @apply text-sm;
+        }
+    }
+}
+</style>
--- src/views/wiki/WikiArticle.vue
+++ src/views/wiki/WikiArticle.vue
@@ -1,15 +1,14 @@
-<script setup>
-import { useRoute, useRouter } from 'vue-router';
+<script setup lang="ts">
 import { reactive } from 'vue';
-import { RouterLink } from 'vue-router';
-import { Marked } from "marked";
-import hljs from 'highlight.js';
-import { markedHighlight } from "marked-highlight";
+import { useRoute, useRouter, RouterLink } from 'vue-router';
+import { marked, type RendererObject } from "marked";
+
+import MarkdownView from '@/components/markdown/MarkdownView.vue';
+
 import { StartLoading, StopLoading } from '@/utils/Loading';
 import Utils from '@/utils/Utils';
 
 const route = useRoute();
-const router = useRouter();
 
 const pathSplit = route.path.split('/');
 const path = pathSplit.slice(2).join('/').toLowerCase() || 'home';
@@ -27,7 +26,18 @@ const pathArray = path.split('/').map(p => {
     };
 });
 
-const react = reactive({
+const react = reactive<{
+    article: string
+    contents: {
+        link: string
+        title: string
+        isH3: boolean
+    }[]
+    path: {
+        link: string
+        title: string
+    }[]
+}>({
     article: '',
     contents: [],
     path: pathArray
@@ -38,11 +48,11 @@ Utils.SetTitle("/" + path);
 
 const lang = "en";
 
-fetch('/fluxis-wiki/' + path + '/' + lang +  '.md')
+fetch('/fluxis-wiki/' + path + '/' + lang + '.md')
     .then(response => response.text())
     .then(render);
 
-function render(data) {
+function render(data: string) {
     if (!data) return render("We couldn't find the page you were looking for.\n\n[Go back to the wiki home page](/wiki/home)");
 
     let title = data.match(/# (.*)/);
@@ -55,8 +65,8 @@ function render(data) {
     if (sections) {
         react.contents = sections.map(section => {
             console.log(section);
-            const link = section.replaceAll("#", '').toLowerCase().trim().replaceAll(" ", '-');
-            const title = section.replaceAll("#", '').trim();
+            const link = section.replace(/#/g, "").toLowerCase().trim().replace(/[^\w]+/g, '-');
+            const title = section.replace(/#/g, "").trim();
 
             return {
                 link: link,
@@ -66,45 +76,30 @@ function render(data) {
         });
     }
 
-    const marked = new Marked(
+    const config: RendererObject = {
+        link: (link) => {
+            if (link.href.startsWith('/')) {
+                return `<RouterLink to="${link.href}">${link.text}</RouterLink>`
+            }
+
+            return false
+        },
+    }
+
+    /* const marked = new Marked(
         markedHighlight({
             langPrefix: 'hljs language-',
             highlight(code, lang, info) {
-              const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-              return hljs.highlight(code, { language }).value;
+                const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+                return hljs.highlight(code, { language }).value;
             }
         })
-    );
+    ); */
 
-    data = marked.parse(data);
-    react.article = data;
+    marked.use({ renderer: config })
+
+    react.article = marked.parse(data).toString();
     StopLoading();
-    setTimeout(postProcess, 100);
-}
-
-function postProcess() {
-    const links = document.querySelectorAll('.article a');
-
-    links.forEach(link => {
-        const url = link.getAttribute('href');
-
-        if (!url.startsWith('/') && !url.startsWith('#')) {
-            link.setAttribute('target', '_blank');
-            return;
-        }
-
-        link.addEventListener('click', event => {
-            event.preventDefault();
-            router.push(url);
-        });
-    });
-
-    if (route.hash) {
-        const h = document.getElementById(route.hash.split("#")[1]);
-        
-        if (h)
-            h.scrollIntoView();
-	}
 }
 
 function edit() {
@@ -118,277 +113,36 @@ function edit() {
 </script>
 
 <template>
-    <div class="wiki">
-        <div class="wiki-header">
-            <div class="wiki-path">
+    <div class="w-full bg-dark-2 rounded-3xl rounded-t-none xl:rounded-t-3xl p-3 pt-4 xl:pt-3">
+        <div class="w-full flex flex-row mb-2 gap-2">
+            <div
+                class="flex flex-row items-center flex-1 px-4 py-2 text-sm text-left bg-dark-3 rounded-lg rounded-t-xl md:rounded-tr-lg">
                 <p>
-                    <router-link to="/wiki/home">Home</router-link>
+                    <router-link class="hover:text-highlight" to="/wiki/home">Home</router-link>
                     <span v-for="section in react.path">
-                        <!-- <i class="fa fa-chevron-right"></i> -->
-                        <span> / </span>
-                        <router-link :to="'/wiki/' + section.link">{{ section.title }}</router-link>
+                        <i class="fa fa-chevron-right text-xs mx-2"></i>
+                        <router-link class="hover:text-highlight" :to="'/wiki/' + section.link">{{ section.title
+                            }}</router-link>
                     </span>
                 </p>
             </div>
-            <a @click="edit">
-                <div class="edit">
-                    <i class="fa fa-github"></i>
-                    Edit this page
-                </div>
+            <a @click="edit"
+                class="hidden md:block px-4 py-2 text-sm text-right bg-primary rounded-lg rounded-tr-xl text-dark-2">
+                <i class="fa fa-github mr-1"></i>
+                Edit this page
             </a>
         </div>
-        <div class="wiki-content">
-            <div class="nav">
-                <h3>Contents</h3>
-                <a v-for="content in react.contents" :href="'#' + content.link">
-                    <p :class="content.isH3 ? 'sub-section' : ''">{{ content.title }}</p>
+        <div class="w-full flex flex-row text-left gap-2">
+            <div class="hidden lg:block w-60 bg-dark-3 rounded-lg rounded-bl-xl p-4">
+                <p class="mb-1 text-sm opacity-80">Contents</p>
+                <a class="text-sm mb-1 hover:text-highlight" v-for="content in react.contents"
+                    :href="'#' + content.link">
+                    <p :class="{ 'pl-4': content.isH3 }">{{ content.title }}</p>
                 </a>
             </div>
-            <div class="article">
-                <div v-if="react.article" v-html="react.article"></div>
+            <div class="flex-1 max-w-full overflow-scroll p-4 bg-dark-3 text-sm rounded-lg rounded-br-xl">
+                <MarkdownView v-if="react.article" :content="react.article" />
             </div>
         </div>
     </div>
 </template>
-
-<style lang="scss">.wiki {
-    width: 100%;
-    background-color: var(--bg-secondary);
-    border-radius: 30px;
-    padding: 10px;
-
-    .wiki-header {
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        margin-bottom: 10px;
-
-        .wiki-path {
-            width: 100%;
-            padding: 10px 20px;
-            font-size: 14px;
-            text-align: left;
-            background-color: var(--bg-triary);
-            border-radius: 20px 10px 10px 10px;
-            margin-right: 10px;
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-
-            i {
-                margin: 0 5px;
-            }
-
-            a:hover {
-                color: var(--highlight);
-            }
-        }
-
-        .edit {
-            padding: 10px 20px;
-            font-size: 14px;
-            text-align: right;
-            background-color: var(--accent-secondary);
-            border-radius: 10px 20px 10px 10px;
-            word-wrap: normal;
-            width: max-content;
-            cursor: pointer;
-
-            i {
-                margin-right: 5px;
-            }
-        }
-    }
-
-    .wiki-content {
-        width: 100%;
-        display: flex;
-        flex-direction: row;
-        text-align: left;
-
-        .nav {
-            width: 400px;
-            margin-right: 10px;
-            background-color: var(--bg-triary);
-            border-radius: 10px 10px 10px 20px;
-            padding: 20px;
-
-            h3 {
-                margin-bottom: 10px;
-            }
-
-            a {
-                display: block;
-                font-size: 14px;
-                margin-bottom: 2px;
-
-                &:hover {
-                    color: var(--highlight);
-                }
-
-                .sub-section {
-                    padding-left: 20px;
-                }
-            }
-        }
-
-        .article {
-            width: 100%;
-            padding: 0 20px 20px 20px;
-            font-size: 14px;
-            line-height: 1.5;
-            background-color: var(--bg-triary);
-            border-radius: 10px 10px 20px 10px;
-
-            h1 {
-                font-size: 1.875rem;
-                line-height: 2.25rem;
-            }
-
-            h2 {
-                font-size: 1.5rem;
-                line-height: 2rem;
-            }
-
-            h3 {
-                font-size: 1.25rem;
-                line-height: 1.75rem;
-            }
-
-            h4 {
-                font-size: 1.125rem;
-                line-height: 1.75rem;
-            }
-
-            h1, h2 {
-                margin-top: 20px;
-                border-bottom: 1px solid rgba($color: #ffffff, $alpha: .4);
-            }
-
-            h3, h4 {
-                margin-top: 15px;
-            }
-
-            h5, h6 {
-                margin-top: 10px;
-            }
-
-            p {
-                font-size: 0.875rem;
-                line-height: 1.25rem;
-
-                margin-top: 10px;
-
-                code {
-                    padding: 2px 5px;
-                }
-            }
-
-            a {
-                color: var(--highlight);
-            }
-
-            ul {
-                margin-top: 10px;
-                margin-left: 20px;
-
-                code {
-                    padding: 2px 5px;
-                }
-            }
-
-            pre {
-                background-color: var(--bg-secondary);
-                padding: 10px;
-                border-radius: 10px;
-                margin-top: 10px;
-                overflow-x: auto;
-            }
-
-            code {
-                background-color: var(--bg-secondary);
-                border-radius: 5px;
-                
-                span {
-                    font-family: monospace;
-                }
-            }
-
-            blockquote {
-                margin-top: 10px;
-                padding: 10px;
-                background-color: var(--bg-secondary);
-                border-left: 5px solid var(--highlight);
-                border-radius: 10px;
-
-                > p:first-child {
-                    margin-top: 0;
-                }
-            }
-
-            img {
-                max-width: 100%;
-                border-radius: 10px;
-                margin-top: 10px;
-            }
-
-            table {
-                margin-top: 10px;
-                border-collapse: collapse;
-
-                img {
-                    margin-top: 0;
-                }
-                
-                th, td {
-                    border: 1px solid rgba($color: #ffffff, $alpha: .4);
-                }
-
-                th {
-                    padding: 5px 20px;
-                }
-
-                td {
-                    padding: 5px 10px;
-                }
-            }
-        }
-    }
-}
-
-@media screen and (max-width: 1000px) {
-    .wiki {
-        .wiki-content {
-            flex-direction: column;
-
-            .nav {
-                display: none;
-            }
-
-            .article {
-                border-radius: 10px 10px 20px 20px;
-            }
-        }
-    } 
-}
-
-@media screen and (max-width: 600px) {
-    .wiki {
-        .wiki-header {
-            flex-direction: column;
-
-            .wiki-path {
-                border-radius: 20px 20px 10px 10px;
-            }
-
-            .edit {
-                display: none;
-            }
-
-            .article {
-                border-radius: 10px 10px 20px 20px;
-            }
-        }
-    } 
-}
-</style>
