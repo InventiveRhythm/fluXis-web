da808ed966ee0fb0ebad3f9affe066a1b7a92354
flustix <me@flux.moe>
2025-08-30 04:04:40 +0200

    implement footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="h-screen w-screen">
-        <div class="mx-auto grid w-full pb-32 pt-20 text-center *:relative *:z-0 *:col-1 *:row-1 xl:w-content xl:pt-28">
+        <div class="mx-auto grid min-h-[calc(100vh-96px)] w-full pb-16 pt-20 text-center *:relative *:z-0 *:col-1 *:row-1 xl:w-content xl:pt-28">
             <slot></slot>
         </div>
 
@@ -9,5 +9,6 @@
         </div>
 
         <NavigationBar />
+        <FooterBar />
     </div>
 </template>
